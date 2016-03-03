b80652675dd1410ca8f752e6d9d0b3754590e657
Eric <schneider@kogosoftwarellc.com>
2016-03-03 16:59:05 -0700

    Styling for large screens

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,12 @@
 $(document).ready(function() {
   $('.menu-button').click(function(event) {
     $('.navigation').toggleClass('opened');
+    $('.overlay').toggleClass('active');
     event.stopPropagation();
   });
 
   $('html').click(function() {
     $('.navigation').removeClass('opened');
+    $('.overlay').removeClass('active').addClass('inactive');
   });
-
-  // $(window).resize(function() {
-  //   $('#header').css('height', $(window).height());
-  // });
 });

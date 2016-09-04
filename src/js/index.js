369375f4384ca35d86e4eb771b542554266c8825
jsdevel <js.developer.undefined@gmail.com>
2016-09-03 23:48:32 -0700

    Updating copy and adding github star counts.

--- src/js/index.js
+++ src/js/index.js
@@ -13,11 +13,10 @@ $(document).ready(function() {
   $.ajax({
     url: 'https://api.github.com/orgs/kogosoftwarellc/repos',
     complete: function(response) {
-      var results = [];
-      response.responseJSON.forEach(function(result) {
-        if (!result.fork) {
-          results.push(result);
-        }
+      var results = response.responseJSON.filter(function(result) {
+        return !result.fork;
+      }).sort(function(a, b) {
+        return b.stargazers_count - a.stargazers_count;
       });
 
       $('.github-counter').text(results.length);
@@ -28,7 +27,13 @@ $(document).ready(function() {
         carouselBase.append($('<div>').html(
           '<div class="carousel-item-container">' +
           '  <div class="carousel-item">' +
-          '    <p class="carousel-item__title">' + result.name + '</p>' +
+          '    <p class="carousel-item__title">' + result.name +
+          '      <span class="carousel-item__star">' +
+          // no space between elements to avoid line breaks
+          '<img src="./images/star.png" height="25px" width="auto">' +
+          '<span>' + result.stargazers_count + '</span>' +
+          '</span>' +
+          '      </p>' +
           '    <p class="carousel-item__description">' + result.description + '</p>' +
           '    <a class="carousel-item__link" href="' + result.html_url  + '">View on Github</a>' +
           '  </div>' +

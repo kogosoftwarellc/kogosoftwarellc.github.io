6820347e232104d7c2c4ed7f231332c5b9793b46
jsdevel <js.developer.undefined@gmail.com>
2017-07-20 14:51:28 -0700

    Fixing div inside anchor, adding scrollTo

--- src/js/index.js
+++ src/js/index.js
@@ -60,4 +60,12 @@ $(document).ready(function() {
       });
     }
   });
+
+  $('a[href^="#"]').click(function(e) {
+    // Prevent the jump and the #hash from appearing on the address bar
+    e.preventDefault();
+    // Scroll the window, stop any previous animation, stop on user manual scroll
+    // Check https://github.com/flesler/jquery.scrollTo for more customizability
+    $(window).stop(true).scrollTo(this.hash, {duration:400});
+  });
 });
